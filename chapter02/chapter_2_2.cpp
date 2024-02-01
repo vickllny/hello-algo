60f20da038c1f376182e5e435bfa75a9b180fc7d
vickllny <vickllny@163.com>
2024-02-01 23:34:27 +0800

    TODO 2.2.2 递归(recurs)

--- chapter02/chapter_2_2.cpp
+++ chapter02/chapter_2_2.cpp
@@ -9,7 +9,29 @@ int for_loop(unsigned n){
     return res;
 }
 
+int while_loop(unsigned n){
+    int res = 0;
+    int i = 0;
+    while(++i <= n){
+        res += i;
+    }
+    return res;
+}
+
+// string nested_for_loop(unsigned n){
+//     ostringstream res;
+//     for (int i = 1; i <= n; i++){
+//         for (int j = 1; j <= n; j++){
+//             res << "(" << i << ", " << j << "), ";
+//         }
+//     }
+//     return res.str();
+// }
+
+//TODO 2.2.2 递归(recurs)
+
 int main(){
-    cout << for_loop(100) << endl;
+    cout << "for_loop => " << for_loop(100) << endl;
+    cout << "while_loop => " << while_loop(100) << endl;
     return 0;
 }
